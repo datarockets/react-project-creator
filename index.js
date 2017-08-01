e137944e2a57ad4534751a818fafa451a4c78848
Vlad Oganov <vlad.oganov@gmail.com>
2017-08-01 13:31:33 +0300

    Set up babel

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const cmd = require('node-cmd');
+import inquirer from 'inquirer';
+import fs       from 'fs';
 
-cmd.get('pwd', (err, data) => console.log('Current directory is: ', data));
+const QUESTIONS = [
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project name:',
+    validate: (input) => {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      else return 'Project name may only include letters, numbers, underscores and hashes.';
+    },
+  },
+];
+
+inquirer
+  .prompt(QUESTIONS)
+  .then((answers) => {
+    console.log(answers);
+  });
