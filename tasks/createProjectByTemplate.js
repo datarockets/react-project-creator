c22a1acfe0ff13b3b620451fed5677df2707b543
Vlad Oganov <vlad.oganov@gmail.com>
2017-08-02 11:29:22 +0300

    Generate files using template project

--- tasks/createProjectByTemplate.js
+++ tasks/createProjectByTemplate.js
@@ -1,9 +1,42 @@
-import fs   from 'fs';
-import _    from 'lodash';
+import fs from 'fs';
+import _  from 'lodash';
 
-const CURR_DIR     = process.cwd();
-const PROJECT_NAME = process.argv[1];
+function getSetupValues() {
+  const currentDir   = process.cwd();
+  const projectName  = process.argv[2];
+  const templatePath = `${__dirname}/templates/react-redux-project`;
+
+  return { currentDir, projectName, templatePath };
+}
+
+function createDirectory(currentDir, projectName) {
+  fs.mkdirSync(`${currentDir}/${projectName}`);
+}
+
+function createDirectoryContents(templatePath, newProjectPath, currentDir) {
+  const filesToCreate = fs.readdirSync(templatePath);
+
+  filesToCreate.forEach((file) => {
+    const originFilePath = `${templatePath}/${file}`;
+    const stats = fs.statSync(originFilePath);
+
+    if (stats.isFile()) {
+      const contents = fs.readFileSync(originFilePath, 'utf8');
+      const writePath = `${currentDir}/${newProjectPath}/${file}`;
+
+      fs.writeFileSync(writePath, contents, 'utf8');
+    }
+
+    if (stats.isDirectory()) {
+      fs.mkdirSync(`${currentDir}/${newProjectPath}/${file}`);
+      createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`, currentDir);
+    }
+  });
+}
 
 export default function run() {
-  console.log(CURR_DIR, PROJECT_NAME);
+  const { currentDir, projectName, templatePath } = getSetupValues();
+
+  createDirectory(currentDir, projectName);
+  createDirectoryContents(templatePath, projectName, currentDir);
 }
