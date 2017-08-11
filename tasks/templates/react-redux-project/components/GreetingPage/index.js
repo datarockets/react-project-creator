6325cc0f9fcca72f7ec53a7ecce25d78d12ee148
Vlad Oganov <vlad.oganov@gmail.com>
2017-08-11 17:56:59 +0300

    Add start page

---
+++ tasks/templates/react-redux-project/components/GreetingPage/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles.css';
+
+export default  class GreetingPage extends Component {
+  render = () => (
+    <div className="main-container">
+      <div className="header">
+        <div className="company-logo-container">
+          <div className="company-logo rounded-edges">
+            <span className="company-logo-alignment">
+              =>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="wrapper">
+        <div className="mr-reactor-logo-wrapper">
+          <img src={logo} className="mr-reactor-logo" alt="mr-reactor-logo" />
+        </div>
+        <p className="greeting">
+          <span className="mr-reactor-span">Mr. Reactor</span> here to help you!
+        </p>
+      </div>
+    </div>
+  );
+}
--- tasks/templates/react-redux-project/index.js
+++ tasks/templates/react-redux-project/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(null, document.getElementById('root'));
+import GreetingPage from 'src/components/GreetingPage';
+
+render(GreetingPage, document.getElementById('root'));
